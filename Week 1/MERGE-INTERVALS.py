8aad812b0e1b4978925bc0e25bd11df98a7762fa
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2021-12-19 09:24:12 +0000

    Create MERGE-INTERVALS.py

---
+++ Week 1/MERGE-INTERVALS.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # intervals = [[0,1],[0,4],[3,1],[-1,4],[-1,-2],[0,6]]
+        intervals.sort()
+        i=0
+        j=0
+        while (i< len(intervals)-1) :
+            if(intervals[i][0]==intervals[i+1][0]):
+                intervals.pop(i)
+                i=i-1
+            i=i+1
+        while j<len(intervals)-1:
+            if(intervals[j][1]>=intervals[j+1][1]):
+                intervals.pop(j+1)
+                continue
+            if(intervals[j][1]>=intervals[j+1][0]):
+                intervals[j][1]=intervals[j+1][1]
+                intervals.pop(j+1)
+                j=j-1
+            j=j+1
+        return intervals   
+    
